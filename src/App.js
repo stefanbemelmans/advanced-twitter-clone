8f6501c68fc8bec1882a5790604187d8e24cdd3d
stackbaxter <stackbaxter@gmail.com>
2017-07-16 11:55:34 -0500

    back to functional...fahking imports

--- src/App.js
+++ src/App.js
@@ -1,17 +1,27 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
+import '../public/twitter.css';
 import './App.css';
 import Body from './components/Body.js';
-// import Header from './components';
-// import Body from './components';
+import Header from './components/Header.js';
+import Profile from './components/Profile.js';
+// import Notification from './components/Notification.js';
+// import Other from './components/Other.js';
 
 
 function App(){
    return (
     <div>
-        <h3>Hello!</h3> 
-      <Body /> 
+      <Header title="Your Title Here" />
+          
+        
+        <div className='flex-grid'>
+        <Profile />
+
+        <Body />
+        <h3>Hello!</h3>  
+      </div>
     </div>
-  );
+    );
  }
 export default App;
--- src/components/Header.js
+++ src/components/Header.js
@@ -1,4 +1,5 @@
 
+import React, { Component }  from 'react';
 function Header(props){
     return(
         <div className="header">
